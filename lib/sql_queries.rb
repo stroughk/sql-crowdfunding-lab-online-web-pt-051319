f5172a355fda3b7b9b84b3174a6f9af844da1ce2
Kateryna Strough <katerynastrough@hotmail.com>
2019-08-03 14:08:17 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -6,11 +6,19 @@
 
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
+<<<<<<< HEAD
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
   "SELECT projects.title, SUM(pledges.amount) FROM projects JOIN pledges ON projects.id = pledges.project_id GROUP BY projects.title;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
+=======
+def selects_the_titles_of_all_projects_and_their_pledge_amounts
+  "SELECT projects.title, SUM(pledges.amount) FROM projects JOIN pledges ON projects.id = pledges.project_id GROUP BY projects.title;"
+end
+
+def selects_the_user_name_age_and_pledge_amount_for_all_pledges
+>>>>>>> cacf7edf7fd544242ecbd957d80cd44c331b6cf6
   "SELECT users.name, users.age, SUM(pledges.amount) FROM users JOIN pledges ON users.id = pledges.user_id GROUP BY users.name;"
 end
 
@@ -18,7 +26,11 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
   "SELECT projects.title, (SUM(pledges.amount) - projects.funding_goal) AS amount_left FROM projects JOIN pledges ON projects.id = pledges.project_id GROUP BY projects.title HAVING amount_left >= 0;"
 end
 
+<<<<<<< HEAD
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
+=======
+def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount
+>>>>>>> cacf7edf7fd544242ecbd957d80cd44c331b6cf6
   "SELECT users.name, SUM(pledges.amount) FROM users JOIN pledges ON users.id = pledges.user_id GROUP BY users.name ORDER BY SUM(pledges.amount);"
 end
 
@@ -26,6 +38,10 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
   "SELECT projects.category, pledges.amount FROM projects JOIN pledges ON projects.id = pledges.project_id WHERE projects.category = 'music';"
 end
 
+<<<<<<< HEAD
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
+=======
+def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_book_category
+>>>>>>> cacf7edf7fd544242ecbd957d80cd44c331b6cf6
   "SELECT projects.category, SUM(pledges.amount) FROM projects JOIN pledges ON projects.id = pledges.project_id WHERE projects.category = 'books' GROUP BY projects.category;"
 end
